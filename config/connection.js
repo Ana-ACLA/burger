5cbeaeacb8a97e073ed15d6b77a32100c2ff8f03
Chris Aja <owner2@anas-mbp.gateway.pace.com>
2017-05-25 21:12:49 -0700

    fixed password in connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
         port: 3306,
         host: "localhost",
         user: "root",
-        password: "Ajanwachuku",
+        password: "Ajanwachuku3$$$",
         database: "burgers_db"
     });
 }
